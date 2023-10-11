ece81b5a4071d9c0c2f6ae33aa54c8494f908cb1
YingJu-Chen-168 <145548518+YingJu-Chen-168@users.noreply.github.com>
2023-10-11 15:14:49 +0800

    Update and rename 1_📈_Plotting_Demo.py to 1_Text_elements.py

---
+++ pages/1_Text_elements.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.header('*Text elements*', divider= 'rainbow')
+st.subheader('不知道要放啥')
+st.caption('_說明說明說明_')
+code= '''def hello():
+print("Hello!")'''
+st.code(code)
+st.divider()
+st.text("寫字寫字")
+st.divider()
+st.latex(r'''a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)''')
