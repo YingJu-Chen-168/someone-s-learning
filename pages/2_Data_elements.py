e9f8bd81efe43fe613289c7e74c9f8112d97596a
YingJu-Chen-168 <145548518+YingJu-Chen-168@users.noreply.github.com>
2023-10-11 15:18:38 +0800

    Create 2_Data_elements.py

---
+++ pages/2_Data_elements.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+
+st.header('*Data elements*', divider= 'rainbow')
+st.subheader('不知道要放啥')
+st.caption('_說明說明說明_')
+df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
+st.dataframe(df.style.highlight_max(axis=0))
+edited_df = st.data_editor(df, num_rows="dynamic")
+st.divider()
+st.caption('_我想放自己的照片_')
+st.divider()
+data_df = pd.DataFrame(
+    {
+        "apps": [
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/ef9a7627-13f2-47e5-8f65-3f69bb38a5c2/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/31b99099-8eae-4ff8-aa89-042895ed3843/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/6a399b09-241e-4ae7-a31f-7640dc1d181e/Home_Page.png",
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "apps": st.column_config.ImageColumn(
+            "Preview Image", help="Streamlit app preview screenshots"
+        )
+    },
+    hide_index=True,
+)
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
