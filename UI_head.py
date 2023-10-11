d5abab53b5e86d27eb422b9b10457f3d3b601e4e
YingJu-Chen-168 <145548518+YingJu-Chen-168@users.noreply.github.com>
2023-10-11 15:11:39 +0800

    Update and rename streamlit_app.py to UI_head.py

---
+++ UI_head.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="User Interface",
+    page_icon="🖋️",
+)
+
+st.title(':violet[人機介面]')
+st.header(divider= 'rainbow')
+st.write("# Welcome! 👋")
+st.text("*這是一個不正經網頁*")
+st.balloons()
