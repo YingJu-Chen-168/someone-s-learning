fde4b1ca7507835cc06c9331425841e9381961b5
YingJu-Chen-168 <145548518+YingJu-Chen-168@users.noreply.github.com>
2023-10-11 15:37:23 +0800

    Update 1_Text_elements.py

--- pages/1_Text_elements.py
+++ pages/1_Text_elements.py
@@ -4,7 +4,8 @@ st.header('*Text elements*', divider= 'rainbow')
 st.subheader('不知道要放啥')
 st.caption('_說明說明說明_')
 code= '''def hello():
-print("Hello!")'''
+    print("Hello!")
+    #大家要記得縮排不然會報錯喔'''
 st.code(code)
 st.divider()
 st.text("寫字寫字")
