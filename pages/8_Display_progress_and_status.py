6432b8edc80c1f27836841ad4cd203951ac97bed
YingJu-Chen-168 <145548518+YingJu-Chen-168@users.noreply.github.com>
2023-10-11 15:30:16 +0800

    Create 8_Display_progress_and_status.py

---
+++ pages/8_Display_progress_and_status.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header('*Display progress and status*', divider= 'rainbow')
+st.subheader('不知道要放啥')
+st.caption('_說明說明說明_')
+st.toast('Your edited image was saved!', icon='😍')
+st.divider()
+st.balloons()
+st.snow()
+st.info('This is a purely informational message', icon="ℹ️")
