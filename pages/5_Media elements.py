138d359fe610f7b7b9a5fa4bde9778404c326bf0
YingJu-Chen-168 <145548518+YingJu-Chen-168@users.noreply.github.com>
2023-10-11 15:47:42 +0800

    Update 5_Media elements.py

--- pages/5_Media elements.py
+++ pages/5_Media elements.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.header('*Media elements*', divider= 'rainbow')
 st.subheader('不知道要放啥')
 st.caption('_說明說明說明_')
-image = Image.open('tutorial.jpg')
+image = Image.open('https://github.com/YingJu-Chen-168/someone-s-learning/blob/main/pages/tutorial.jpg')
 st.image(image, caption='可愛貓頭鷹')
