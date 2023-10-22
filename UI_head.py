c0c89b8d62cd4b84ca795b830b6d2abf8b967b8d
YingJu-Chen-168 <145548518+YingJu-Chen-168@users.noreply.github.com>
2023-10-22 15:22:45 +0800

    Update UI_head.py

--- UI_head.py
+++ UI_head.py
@@ -5,8 +5,8 @@ st.set_page_config(
     page_icon="🖋️",
 )
 
-st.title(':violet[人機介面]')
-st.header('耶', divider= 'rainbow')
+st.title('人機介面')
+st.header('demo', divider= 'rainbow')
 st.write("# Welcome! 👋")
-st.text("*這是一個不正經網頁*")
+#st.text("*這是一個不正經網頁*")
 st.balloons()
